b69d997af0e343472743a2f6c6f7e39f9837d824
Thuan Nguyen <thuan.nguuyen@yoot.vn>
2023-08-16 10:14:20 +0700

    raw logic of nodejs

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer(function(req, res) {
-   res.setHeader('Content-Type','text/html');
-   res.write('<html>');
-   res.write('<head><title>My First page</title></head>');
-   res.write('<body><h1>My First page</h1></body>');
-   res.write('</html>');
+const server = http.createServer(function (req, res) {
+  const url = req.url;
+  const method = req.method;
+  
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type = "Submit">Submit</button></form></body>'
+    );
+    res.write("</html>");
 
-   res.end();
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    })
 
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync("message.txt", message);
+    })
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First page</title></head>");
+  res.write("<body><h1>My First page</h1></body>");
+  res.write("</html>");
+
+  res.end();
 });
 
 server.listen(3000);
-
